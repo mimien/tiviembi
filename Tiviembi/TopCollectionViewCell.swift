3736d1607cf73928024204aaeb17e3d12563199e
Emilio Cornejo <emiliocornejo2@gmail.com>
2016-05-02 18:52:24 -0500

    create a top storyboard added

---
+++ Tiviembi/TopCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TopCollectionViewCell.swift
+//  Tiviembi
+//
+//  Created by Emilio Cornejo on 02/05/16.
+//  Copyright © 2016 Emilio Cornejo. All rights reserved.
+//
+
+import UIKit
+
+class TopCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var title: UILabel!
+}
+
--- Tiviembi/TopsViewController.swift
+++ Tiviembi/TopsViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class TopsViewController: UIViewController {
+class TopsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +23,35 @@ class TopsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func createTop(sender: AnyObject) {
+        
+    }
     
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! TopCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.title.text = self.items[indexPath.item]
+        cell.layer.borderColor = UIColor.lightGrayColor().CGColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 4
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
     }
 }
